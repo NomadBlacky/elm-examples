896ec6449ee0acb10ea710396f978248556df251
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-03-02 01:18:24 +0900

    p198 elm-test のサンプルコードを追加

---
+++ tests/Example.elm
@@ -0,0 +1,40 @@
+module Example exposing (suite)
+
+import Expect exposing (..)
+import Fuzz exposing (..)
+import Test exposing (..)
+
+
+
+-- https://package.elm-lang.org/packages/elm-explorations/test/latest
+
+
+suite : Test
+suite =
+    describe "The String module"
+        [ describe "String.reverse"
+            -- Nest as many descriptions as you like.
+            [ test "has no effect on a palindrome" <|
+                \_ ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                    Expect.equal palindrome (String.reverse palindrome)
+
+            -- Expect.equal is designed to be used in pipeline style, like this.
+            , test "reverses a known string" <|
+                \_ ->
+                    "ABCDEFG"
+                        |> String.reverse
+                        |> Expect.equal "GFEDCBA"
+
+            -- fuzz runs the test 100 times with randomly-generated inputs!
+            , fuzz string "restores the original string if you run it again" <|
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
+        ]
